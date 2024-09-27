08963d6b3e18a5688b8f64d4ffe16faa5afa861e
NachammaiM05 <132426594+NachammaiM05@users.noreply.github.com>
2024-09-27 09:11:50 +0530

    Update archived_index.php

--- archived_index.php
+++ archived_index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Home | LoanHub</title>
+  <title> Archived_Index | LoanHub</title>
 
 <?php
   session_start();
